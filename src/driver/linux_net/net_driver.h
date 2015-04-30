35d9f1966e91fdf971721e7f203f92f0939279c5
Marek Majkowski <marek@cloudflare.com>
2015-04-30 12:27:17 +0100

    bug52514

--- src/driver/linux_net/net_driver.h
+++ src/driver/linux_net/net_driver.h
@@ -80,7 +80,7 @@
  *
  **************************************************************************/
 
-#define EFX_DRIVER_VERSION	"4.1.2.1003b"
+#define EFX_DRIVER_VERSION	"4.1.2.1003b_bug52514"
 
 #ifdef DEBUG
 #define EFX_BUG_ON_PARANOID(x) BUG_ON(x)
